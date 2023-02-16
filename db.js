7cc8b64902ba9937099994840ace529104a8ed8a
ericpkatz <ericpkatz@gmail.com>
2023-02-15 20:30:05 -0500

    set up other models

--- db.js
+++ db.js
@@ -9,18 +9,45 @@ const Person = conn.define('person', {
   }
 });
 
+const Thing = conn.define('thing', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
+});
+
+const Place = conn.define('place', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  }
+});
+
 const Souvenir = conn.define('souvenir', {
 });
 
 Souvenir.belongsTo(Person);
 Person.hasMany(Souvenir);
 
+Souvenir.belongsTo(Place);
+Place.hasMany(Souvenir);
+
+Souvenir.belongsTo(Thing);
+Thing.hasMany(Souvenir);
+
 
 module.exports = {
   conn,
   Person,
-  Souvenir
+  Souvenir,
+  Thing,
+  Place
 }
-
-
-
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const db = require('./db');
-const { Souvenir, Person, conn } = db;
+const { Place, Thing, Souvenir, Person, conn } = db;
 
 
 const port = process.env.PORT || 3000;
@@ -10,16 +10,37 @@ app.listen(port, async()=> {
   try {
     console.log(`listening on port ${port}`);
     await conn.sync({ force: true });
-    const [ lucy, moe, larry, ethyl ] = await Promise.all([
+    const [
+      lucy,
+      moe,
+      larry,
+      ethyl,
+      foo,
+      bar,
+      bazz,
+      quq,
+      nyc,
+      paris,
+      london,
+      la
+    ] = await Promise.all([
       Person.create({ name: 'lucy' }),
       Person.create({ name: 'moe' }),
       Person.create({ name: 'larry' }),
       Person.create({ name: 'ethyl' }),
+      Thing.create({ name: 'foo' }),
+      Thing.create({ name: 'bar' }),
+      Thing.create({ name: 'bazz' }),
+      Thing.create({ name: 'quq' }),
+      Place.create({ name: 'NYC' }),
+      Place.create({ name: 'Paris' }),
+      Place.create({ name: 'London' }),
+      Place.create({ name: 'LA' }),
     ]);
 
     await Promise.all([
-      Souvenir.create({ personId: lucy.id }),
-      Souvenir.create( { personId: lucy.id }),
+      Souvenir.create({ personId: lucy.id, thingId: foo.id, placeId: london.id }),
+      Souvenir.create( { personId: lucy.id, thingId: foo.id, placeId: la.id}),
     ]);
   }
   catch(ex){
